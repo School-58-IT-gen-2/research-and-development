7e1248640d187aec68f77c54910f60ff06f1190a
TimofeyYA1 <timdim2018@mail.ru>
2024-11-10 15:37:49 +0300

    data

---
+++ Classes.py
@@ -0,0 +1,62 @@
+class MainInfo():
+    def __init__(self):
+        pass
+    name = ''
+    category = ''
+    level = 1
+    race = ''
+    worldview = ''
+
+
+class Characteristic():
+    def __init__(self):
+        pass
+    STR = 1
+    DEX = 1
+    CON = 1
+    INT = 1
+    WIS = 1
+    CHA = 1
+
+class Abilities():
+    def __init__(self):
+        pass
+    skills = []
+    combat_abilities = []
+    special_features = []
+
+class PointsAndHealth():
+    def __init__(self):
+        pass
+    health_points = 1
+    health_cubes = 1
+    experience_points = 1
+
+class DefenseAndAttack():
+    def __init__(self):
+        pass
+    AC = 1
+    initiative = 1
+    movement_speed = 1
+    AttacksAndSpells = []
+
+class EquipmentAndInventory():
+    def __init__(self):
+        pass
+    weapons_and_armor = []
+    items_and_supplies = []
+    money = []
+
+class PrehistoryAndPersonality():
+    def __init__(self):
+        pass
+    traits = []
+    moral_values = []
+    history_and_origin = []
+    weaknesses_and_disadvantages = []
+class AdditionalInformation():
+    def __init__(self):
+        pass
+    languages = []
+    saving_throws = []
+    features_and_traits = []
