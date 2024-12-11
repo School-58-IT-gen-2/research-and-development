2600e7d88fa2300d070fd736f8c0aee2b43ecfb9
ghjcnj0 <artem.chetokin@gmail.com>
2024-12-11 18:47:40 +0300

    +test

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 from fastapi.testclient import TestClient
-from main import app, choose  
+from collections import defaultdict
 import json
+from main import app, choose
 
 # Создаём экземпляр TestClient
 client = TestClient(app)
 
 gender = ['M', 'W']
-# races = ['dwarf','elves',"halfling","human","dragonborn","gnom","halfelf","halforc","tiefling"]
-races = {"Дварф":'dwarf',"Эльф":'elves','Полурослик':"halfling",'Человек':"human",'Драконорожденный':"dragonborn",'Гном':"gnom",'Полуэльф':"halfelf",'Полуорк':"halforc",'Тифлинг':"tiefling"}
+races = {"Дварф": 'dwarf', "Эльф": 'elves', 'Полурослик': "halfling", 'Человек': "human", 'Драконорожденный': "dragonborn",
+         'Гном': "gnom", 'Полуэльф': "halfelf", 'Полуорк': "halforc", 'Тифлинг': "tiefling"}
 
 keys = list(races.keys())
 
+check = defaultdict(int)
 
 for g in gender:
     for r in keys:
@@ -19,54 +21,64 @@ for g in gender:
 
         class_options = d.get('class_options', [])
         for c in class_options:
-            for _ in range(5):
-                data = {
-                            "gender": f"{g}",
-                            "rac": f"{r}",
-                            "clas": f"{c}"
-                        }
-                response = client.post("/register/", params=data)
+            with open('test_player.json', 'r') as f:
+                json_data1 = json.load(f)
 
-           
-                print(response.status_code, g, r, c)
-            # print(g, r, c)
+            data = {
+                "gender": f"{g}",
+                "rac": f"{r}",
+                "clas": f"{c}"
+            }
 
+            response = client.post("/register/", params=data)
+
+            json_data2 = response.json()
+
+            # Сравнение по ключам
+            keys1 = json_data1.keys()
+            keys2 = json_data2.keys()
+
+            common_keys = set(keys1) & set(keys2)  # пересечение ключей
+
+            for key in common_keys:
+                if json_data1[key] == json_data2[key]:
+                    check[key] += 1
+
+
+print(check)
 print('end')
 
-# 5
-# 200 M Дварф Жрец
-# 200 M Дварф Воин
-# 200 M Дварф Паладин
-# 200 M Дварф Следопыт
-# 200 M Эльф Следопыт
-# 200 M Эльф Маг
-# 200 M Эльф Колдун
-# 200 M Эльф Бард
-# 200 M Эльф Плут
-# 9
 
 
-# 1
-# 200 M Дварф Жрец
-# 200 M Дварф Воин   
-# 200 M Дварф Паладин
-# 200 M Дварф Следопыт
-# 200 M Эльф Следопыт
-# 200 M Эльф Маг     
-# 200 M Эльф Колдун
-# 200 M Эльф Бард
-# 200 M Эльф Плут
-# 200 M Полурослик Плут
-# 200 M Полурослик Жрец    
-# 200 M Полурослик Следопыт
-# 200 M Полурослик Монах
-# 200 M Человек Воин
-# 200 M Человек Жрец
-# 200 M Человек Маг
-# 200 M Человек Паладин
-# 200 M Человек Следопыт
-# 200 M Человек Бард
-# 19
+# 4
+
+# gender = ['M', 'W']
+# # races = ['dwarf','elves',"halfling","human","dragonborn","gnom","halfelf","halforc","tiefling"]
+# races = {"Дварф":'dwarf',"Эльф":'elves','Полурослик':"halfling",'Человек':"human",'Драконорожденный':"dragonborn",'Гном':"gnom",'Полуэльф':"halfelf",'Полуорк':"halforc",'Тифлинг':"tiefling"}
+
+# keys = list(races.keys())
+
+
+# for g in gender:
+#     for r in keys:
+#         with open(f"race/{races.get(r)}.json", "r", encoding="utf-8") as file:
+#             d = json.load(file)
+
+#         class_options = d.get('class_options', [])
+#         for c in class_options:
+#             for _ in range(5):
+#                 data = {
+#                             "gender": f"{g}",
+#                             "rac": f"{r}",
+#                             "clas": f"{c}"
+#                         }
+#                 response = client.post("/register/", params=data)
+
+           
+#                 print(response.status_code, g, r, c)
+#             # print(g, r, c)
+
+# print('end')
 
 
 # 3
