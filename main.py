39ede872a4b4cc126f6854987c89bb81fac77cd7
TimofeyYA1 <timdim2018@mail.ru>
2024-12-15 16:50:18 +0300

    ц

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from db_source import DBSource
 clases = {'Следопыт':'pathfinder',"Варвар":"barbarian","Бард":"bard","Плут":"dodger","Друид":"druid","Колдун":"magician","Монах":"monk","Паладин":"paladin","Жрец":"priest","Маг":"warlock","Воин":"warrior","Волшебник":"wizzard"}
 races = {"Дварф":'dwarf',"Эльф":'elves','Полурослик':"halfling",'Человек':"human",'Драконорожденный':"dragonborn",'Гном':"gnom",'Полуэльф':"halfelf",'Полуорк':"halforc",'Тифлинг':"tiefling"}
 
-supabase = DBSource('https://supabase.questhub.pro/',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzMwMzIyMDAwLAogICJleHAiOiAxODg4MDg4NDAwCn0.oaSCoTPKV6H1XJ_7eWgl67oxnfav4KnNXu8KUkNROJs")
+supabase = DBSource('https://supabase.questhub.pro/',"")
 supabase.connect()
 
 with open('player.json', 'r', encoding="utf-8") as player_list: 
