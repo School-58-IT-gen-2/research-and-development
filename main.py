cb441f0a094f10649b2f886559deb3641dd0dc95
Lev Kerskiy-Gosling <levker08@icloud.com>
2025-03-26 22:19:49 +0300

    ЖЕСТКИЙ БАГ ФИКС

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import uvicorn
 from tg_bot.bot import main
 
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         app="rnd_fastapi_server.server:app",
-#         host="0.0.0.0",   
-#         port=6000,
-#         reload=True,
-#     )
+if __name__ == "__main__":
+    uvicorn.run(
+        app="rnd_fastapi_server.server:app",
+        host="0.0.0.0",   
+        port=6000,
+        reload=True,
+    )
 main()
