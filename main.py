9d0aaf9ec6614f22f1964be75f5fc5e97c770762
Lev Kerskiy-Gosling <levker08@icloud.com>
2025-03-27 19:35:00 +0300

    все вернул на старый порт и вроде все теперь работает

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app="rnd_fastapi_server.server:app",
         host="0.0.0.0",   
-        port=8000,
+        port=6000,
         reload=True,
     )
 main()
